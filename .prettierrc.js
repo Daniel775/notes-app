898192ba51e97db9e43aa5d38057ba4727669543
Daniel M. Bomfim <daniellsmv@hotmail.com>
2023-10-01 15:13:01 -0300

    chore: update prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+	arrowParens: 'avoid',
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 80
 };
