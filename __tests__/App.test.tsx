31bc778c7c5255b7fd907da8c88e0b4db2678b44
Daniel M. Bomfim <daniellsmv@hotmail.com>
2023-10-02 17:40:28 -0300

    test: fix error with react-native-splash-screen on jest

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,15 @@
 import Realm from 'realm';
-import { it, afterAll } from '@jest/globals';
+import { it, afterAll, jest } from '@jest/globals';
 import { render, renderHook, waitFor } from '@testing-library/react-native';
 import App from '../App';
 import RealmContext from '../src/services/database';
+
+jest.mock('react-native-splash-screen', () => {
+	return {
+		hide: jest.fn(),
+		show: jest.fn(),
+	};
+});
 
 afterAll(() => {
 	const wrapper = ({ children }: { children: JSX.Element }) => (
